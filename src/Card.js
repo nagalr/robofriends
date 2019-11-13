69c3d9af030eb627a5eee467070b9097602e2459
Ronnen Nagal <nagalr@gmail.com>
2019-11-13 18:44:49 -0500

    Delete files and add Carrd.js

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="" alt=""/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
+
