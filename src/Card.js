e11036004da64370cf7a7805589a2d15c8af0e59
Ronnen Nagal <nagalr@gmail.com>
2019-11-13 20:48:56 -0500

    Changed Card.js, and index.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,30 @@
+// must import that in order to use JSX, the HTML-like code
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+    // we can only return one thing from Component
+    // here, it's the <div> (we cannot add more)
     return (
-        <div>
-            <img src="" alt=""/>
+        /*
+         The styling of the Cards will be inline here, using tachyons elements
+         (After the 'className' there are built-in tachyons elements, like in bootstrap)
+         if the styling is big, we can add CSS file (Card.css)
+         the { props } are in brackets since it's js code
+         without the brackets, it will show 'props.name' as-is
+         The img src=.. is like that:
+         we replaced the " sign with ` (template string) to wrap the url
+         we wrapped the url with {} since it's a javascript expression
+         robohash.org/randomText - can produce random robot (from the api)
+         we replaced the static random text with 'props.id' (different for each robot)
+         so every robot will have a unique robot picture, $ - for the value
+         {} - since it's js expression
+         ?200x200 - the size of the robot as defined in the api
+        */
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{ props.name }</h2>
+                <p>{ props.email }</p>
             </div>
         </div>
     );
@@ -14,3 +32,18 @@ const Card = () => {
 
 export default Card;
 
+/*
+we could start (line before the return) with:
+const { name, email, id} = props;
+So, the values of name, email, id - will be taken from props,
+and instead of writing 'props.name' we can write 'name' only,
+same for email and id, wherever they appear.
+
+Another thing that we can do: (destructuring)
+Instead of: const Card = (props) => { ..
+We can write: const Card = ({ name, email, id }) => {...
+we receiving props and destructuring the props in the brackets -
+the values that the function receives.
+(appears in Lecture number 205)
+and use the same: 'name', 'email', 'id' as in the code above.
+ */
--- src/index.js
+++ src/index.js
@@ -5,13 +5,28 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Card from './Card';
 import 'tachyons';
+// we us { robots } since we have few exports (from robots.js)
+import { robots } from './robots';
 
+ReactDOM.render(
+                /*
+                 we can only return one element, so we wrap
+                 few Card-Components in a <div>
+                 we will add properties to each Card
+                */
+                <div>
+                    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+                    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+                    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+                </div>
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// The Default used to be register(),
-// now it's unregister() - we will not use serviceWorker Until we want to.
+/*
+ If you want your app to work offline and load faster, you can change
+ unregister() to register() below. Note this comes with some pitfalls.
+ Learn more about service workers: https://bit.ly/CRA-PWA
+ The Default used to be register(),
+ now it's unregister() - we will not use serviceWorker Until we want to.
+*/
 serviceWorker.unregister();
