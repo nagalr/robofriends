fc5a560a60beaf6474f4caf638edb6928d0a0e81
Ronnen Nagal <nagalr@gmail.com>
2019-11-29 15:39:20 -0500

    Add 'onSearchChange' function the App.js and change the function into a class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component} from 'react';
 import CardList from "./CardList";
 import {robots} from "./robots";
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+
+class App extends Component{
+    constructor () {
+        super ()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+
+    }
+
+    onSearchChange (event) {
+        console.log(event.target.value);
+    }
+
+    render ()
+    {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        );
+    };
 };
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const searchBox = () => {
+const searchBox = ({ searchChange }) => {
     return (
         <div className='p2'>
             <input
                 className='pa3 ba b--green hover-bg-lightest-blue'
                 type='search'
                 placeholder='search robots'
+                onChange = { searchChange }
             />
         </div>
     );
